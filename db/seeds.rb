172027d66fead49196132aeb1d785547a76d5c18
Terry Yoder <terry.yoder@carefirst.com>
2023-01-31 07:53:18 -0800

    initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Adding demo sharks
+sharks = Shark.create([{ name: 'Great White', facts: 'Scary' }, { name: 'Megalodon', facts: 'Ancient' }, { name: 'Hammerhead', facts: 'Hammer-like' }, { name: 'Speartooth', facts: 'Endangered' }])
+Post.create(body: 'These sharks are misunderstood', shark: sharks.first)
